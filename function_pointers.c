d8023cb571cbf0e6c874ca2a84c97bef8f938f8b
Danilo Novakovic <dakenzi97@gmail.com>
2017-02-14 12:07:03 +0100

    Applied ex. 5-14 to 5-17 from K&R
    I added exercises 5-14, 5-15, 5-16, 5-17 from K&R Programming in C 2nd
    Edition book to my modified example of pointers to functions on simpler
    algorithm to understand then Quick Sort.

--- function_pointers.c
+++ function_pointers.c
@@ -1,34 +1,60 @@
 /*Example of pointer to function usage in C programming language.
 	Idea taken from K&R Programming in C book, but i made a more modern
-	approach that is easier to understand with simpler algorithm.*/
+	approach that is easier to understand with simpler algorithm.
+	(ex. 5-14,15,16,17 applied)*/
 	
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
-#define MAX_WORDS 13
-#define MAX_LENGTH 21
 
 int get_words(char *[], int);
-int get_word(char *, int); //returns 1  if '\n' is seen
+int get_word(char *, int); 					// returns 1  if '\n' is seen
 void bubble_sort(void *[], int, int (*)(void *, void *));
 void swap(void *[], int i, int);
 void print_words(char *[], int);
-int numcmp(void *, void *); //compares 2 strings nummericaly
+int numcmp(char *, char *); 			// compares 2 strings nummericaly
+int mystrcmp(char *, char *);
+
+#define MAX_WORDS 13
+#define MAX_LENGTH 21
+
+int decreasing = 0;      // 0 if increasing, 1 if decreasing   -r flag 
+int numeric = 0;          // 1 if numeric sort   -n flag 
+int fold = 0;            	  // 1 if not case-sensitive   -f flag 
+int directory = 0;        // 1 if directory sort   -d flag 
 
 int main(int argc, char *argv[]) 
 {
 	char *words[MAX_WORDS];
-	int n, numeric = 0;
+	int n, i;
+	
+	while(--argc > 0) {
+		++argv;
+		if((*argv)[0] == '-') {
+			for(i = 1; (*argv)[i] ; i++)
+				switch((*argv)[i]) {
+					case 'n' : numeric = 1;	   break;
+					case 'r' : decreasing = 1; break;
+					case 'f' : fold = 1;		   break;
+					case 'd' : directory = 1;   break;
+					default : puts("usage: ./a.out -dfnr");
+								   return EXIT_FAILURE;
+				}
+		}
+		else {
+			puts("usage: ./a.out -dfnr");
+			return EXIT_FAILURE;
+		}
+	}
 	
 	puts("Type text:");
 	n = get_words(words, MAX_WORDS);
 	
 	puts("Sorted:");
-	if (argc > 1 && strcmp(argv[1], "-n") == 0) //ex. "./a.out -n"
-		numeric = 1;
+
 	bubble_sort((void **) words, n,  
-		(int (*)(void *, void *)) (numeric ? numcmp : strcmp));
+		(int (*)(void *, void *)) (numeric ? numcmp : mystrcmp));
 	
 	/*int (*)(void *, void *) part is explicit conversion of functions, kinda like 
 		if i would have written (double) 3 /2 expression. */
@@ -45,15 +71,21 @@ void bubble_sort(void *A[], int n, int (*func) (void *, void *))
 	for(i = 0; i < n; i++) {
 		sorted = 1;
 		for(j = 0; j < n - 1; j++) 
-			if((*func)(A[j], A[j+1]) > 0) { 
-				swap(A,  j,  j+1);
-				sorted = 0;
+			switch(decreasing) {
+					case 0 : if((*func)(A[j], A[j+1]) > 0) { 
+									swap( A,  j,  j+1);
+									sorted = 0;
+								 } break;
+					case 1 : if((*func)(A[j], A[j+1]) < 0) { 
+									swap( A,  j,  j+1);
+									sorted = 0;
+								 } break;		
 			}
+			
 		if(sorted) 
 			return;
 	}
 }
-
 void swap(void *A[], int i, int j)
 {
 	void *temp;
@@ -63,7 +95,26 @@ void swap(void *A[], int i, int j)
 	A[j] = temp;
 }
 
-int numcmp(void *s1, void *s2)
+int mystrcmp(char *s1, char *s2) 
+{
+	//i avoided white chars in get_word function anyway.
+	
+	while( fold ? (tolower(*s1) == tolower(*s2)) : *s1 == *s2) {
+		if(!*s1) 
+			return 0;
+		s1++;
+		s2++;
+		if(directory) {
+			while(!isalpha(*s1) && !isdigit(*s1) && *s1)  
+				s1++; 
+			while(!isalpha(*s2) && !isdigit(*s2) && *s2)
+				s2++;
+		}
+	}
+	return  fold ? (tolower(*s1) - tolower(*s2)) : *s1 - *s2;
+}
+
+int numcmp(char *s1, char *s2)
 {
 	double v1, v2;
 	
@@ -97,7 +148,7 @@ int get_words(char *words[], int n)
 
 int get_word(char *word, int n) 
 {
-	while(isspace(*word = getchar()) //avoids white spaces until first non-white char 
+	while(isspace(*word = getchar())) //avoids white spaces until first non-white char 
 		if(*word == '\n') {
 			*word = '\0';
 			return 1;
@@ -113,7 +164,8 @@ int get_word(char *word, int n)
 	return c == '\n';
 }
 
-void print_words(char *A[], int n) {
+void print_words(char *A[], int n) 
+{
 	while(--n >= 0) 
 		printf((n > 0) ? "%s " : "%s\n", *A++);
 }
