0d73c7eea07d70428ee96d5520881e1196f39ef2
DaniloNovakovic <dakenzi97@gmail.com>
2017-05-18 02:29:13 +0200

    chess v1.11b

--- chess/chess.c
+++ chess/chess.c
@@ -1,11 +1,28 @@
-/*Chess made in C programming language (serbian piece names). 
+/*
+	Chess in C programming language.
+    Copyright (C) 2017  Danilo Novaković
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
  K - kralj (king), 
  D - dama(queen), 
  T - top (rook), 
  L - lovac (bishop), 
  S - skakac/konj (knight), 
  P - pesak (pawn)
- Autor: Danilo Novakovic */
+ 
+ */
 #include "list.h"
 #include "chess.h"
 #include "redo_undo.h"
--- chess/menu.c
+++ chess/menu.c
@@ -1,18 +1,35 @@
+/*
+	Chess in C programming language.
+    Copyright (C) 2017  Danilo Novaković
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 #include "menu.h"
 //returns 1 if person wants to exit the program
 int print_menu() {
 	system("tput reset");
-	puts("### Chess ###");
-	puts("Author: Danilo Novakovic FTN Novi Sad");
+	puts("\x1b[32;4;1m### Chess ###\x1b[0m");
+	puts("\x1b[35;1m Author: Danilo Novakovic FTN Novi Sad\x1b[0m");
 	puts("");
 	char  option;
 	do {		
-		puts("Main Menu");
+		puts("\x1b[4mMain Menu\x1b[0m");
 		puts("\t1. Start");
 		puts("\t2. Rules | Help");
-		puts("\tx  - exit");
+		puts("\x1b[31;1m\tx  - exit\x1b[0m");
 		
-		printf("\noption: ");
+		printf("\n\x1b[4mOption\x1b[0m: ");
 		scanf(" %c", &option);
 		switch(option) {
 			case '2': chess_rules(); break;
