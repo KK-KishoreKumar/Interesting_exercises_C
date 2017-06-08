47fc3371ca82ea401306e2a9e767c55caddb1b1e
DaniloNovakovic <dakenzi97@gmail.com>
2017-06-08 21:27:52 +0200

    Find Primes in interval <lower_bound> to <higher_bound>
    Updated primes.c file with comments and changed from atoi to strtoul
    (since i'm working with unsigned numbers)

--- primes.c
+++ primes.c
@@ -1,31 +1,34 @@
-/* My humble alghorithm for finding prime numbers in certain interval*/
+/* My humble alghorithm for finding prime numbers in certain interval.
+   It prints aprox 125 milion primes per minute. 
+*/
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
 unsigned main(int nb_arg, char *args[]) {
-    FILE *pout = fopen("primes.txt", "w");
+	FILE *pout = fopen("primes.txt", "w");
 	if(nb_arg < 3) {
 		puts("usage: <program_name> <lower_bound> <higher_bound>");
 		return EXIT_FAILURE;
-    }
-    unsigned upper_bound = atoi(args[2]),  lower_bound = atoi(args[1]);
+    	}
+	unsigned upper_bound = strtoul(args[2], NULL, 10),  lower_bound = strtoul(args[1], NULL, 10);
 	if(upper_bound < lower_bound) {
 		puts("error: <higher_bound> must be higher then <lower_bound>");
 		return EXIT_FAILURE;
 	}
-    unsigned max_test = (unsigned) sqrt((double)upper_bound) + 1;
+	unsigned max_test = (unsigned) sqrt((double)upper_bound) + 1;
 	unsigned primes[max_test];
 	unsigned *left, *right = primes;
-    *right++ = 2;
-    *right++ = 3;
-    *right++ = 5;
-     *right++ = 7;
-     *right = 11;
+	*right++ = 2;
+	*right++ = 3;
+	*right++ = 5;
+ 	*right++ = 7;
+	*right = 11;
+	
 //TODO: Find max_test primes  
     unsigned temp;
 	unsigned char truth;
-    unsigned square;
+   	unsigned square;
 	left = primes;
 	while(left <= right) {
 		if(lower_bound < *left) fprintf(pout, "%u\n", *left);
@@ -51,23 +54,29 @@ unsigned main(int nb_arg, char *args[]) {
 			}
 		}
 	}
-	if((lower_bound & 1) == 0) lower_bound++;
+	/*
+	TODO: Test prime numbers from <lower_bound> to <upper_bound>
+	note: Before start of loop i change number to odd number (if it is even).
+	(this way i double the speed of alghorithm by checking only odd numbers (i+=2), instead of waisting time on even numbers 
+	(all even numbers are divisable by 2)
+	*/
+	if((lower_bound & 1) == 0) lower_bound++; 
 	for(temp = lower_bound; temp < upper_bound; temp+=2) {
-        square = (unsigned)sqrt((double)temp) + 1;
-        truth ^= truth;
-		truth++;	
-        for(left = &primes[1]; *left < square; left++) {
-		   if(left > right) break;
-		   if(temp%(*left) == 0) { 
-                truth ^= truth;
-                break;
-            }
-        }
-        if(truth) {
-            if(temp < max_test) *++right = temp;
-			fprintf(pout, "%u\n", temp);
-        }
-    }
+        	square = (unsigned)sqrt((double)temp) + 1;
+        	truth ^= truth;
+			truth++;	
+        	for(left = &primes[1]; *left < square; left++) {
+				if(left > right) break;
+				if(temp%(*left) == 0) { 
+                		truth ^= truth;
+                		break;
+            		}
+        	}
+        	if(truth) {
+				if(temp < max_test) *++right = temp;
+				fprintf(pout, "%u\n", temp);
+        	}
+    	}
 	fclose(pout);
 	
     return EXIT_SUCCESS;
